37569121fdfadc7cf5d2795cc448832b6b9bd078
derrikf2 <fleminde@mail.gvsu.edu>
2018-10-15 00:27:38 -0400

    java setup: here goes nothing

---
+++ projects/project01/FTPServer.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class FTPServer {
+    String fromClient;
+
+    public static void main(String argv[]) throws Exception {
+        String clientCommand;
+        byte[] data;
+
+        ServerSocket welcomeSocket = new ServerSocket(12000);
+        String frstln;
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            fromClient = inFromClient.readLine();
+
+            StringTokenizer tokens = new StringTokenizer(fromClient);
+            frstln = tokens.nextToken();
+            port = Integer.parseInt(frstln);
+            clientCommand = tokens.nextToken();
+
+            if (clientCommand.equals("list:")) {
+
+                Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                DataOutputStream dataOutToClient = new DataOutputStrea(dataSocket.getOutputStream());
+                // ..........................
+
+            }
+
+            dataSocket.close();
+            System.out.println("Data Socket closed");
+        }
+
+        // ......................
+
+        if (clientCommand.equals("retr:")) {
+            // ............................................................
+        }
+    }
+}
