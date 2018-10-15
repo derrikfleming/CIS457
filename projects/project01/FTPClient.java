f6b39812aa98ed2f956d662f6e59c067b2e91406
derrikf2 <fleminde@mail.gvsu.edu>
2018-10-15 08:19:45 -0400

    clean up FTPClient loop

--- projects/project01/FTPClient.java
+++ projects/project01/FTPClient.java
@@ -15,8 +15,6 @@ class FTPClient {
         boolean notEnd = true;
         String statusCode;
         boolean clientgo = true;
-        int controlPort = 9381;
-        int port = port1;
 
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         sentence = inFromUser.readLine();
@@ -25,17 +23,21 @@ class FTPClient {
         if (sentence.startsWith("connect")) {
             String serverName = tokens.nextToken(); // pass the connect command
             serverName = tokens.nextToken();
-            controlPort = Integer.parseInt(tokens.nextToken());
-            System.out.println("You are connected to " + serverName);
+            int controlPort = Integer.parseInt(tokens.nextToken());
+            int port = controlPort;
+            System.out.println("You are connecting to " + serverName + "\n");
+            System.out.println("On port:  " + controlPort + "\n");
 
-            Socket ControlSocket = new Socket(serverName, controlPort);
+            Socket controlSocket = new Socket(serverName, controlPort);
+
+            System.out.println("You are connected to " + serverName + ":" + controlPort);
 
             while (isOpen && clientgo) {
 
-                DataOutputStream outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+                DataOutputStream outToServer = new DataOutputStream(controlSocket.getOutputStream());
 
                 DataInputStream inFromServer = new DataInputStream(
-                        new BufferedInputStream(ControlSocket.getInputStream()));
+                        new BufferedInputStream(controlSocket.getInputStream()));
 
                 sentence = inFromUser.readLine();
 
@@ -51,21 +53,40 @@ class FTPClient {
                     while (notEnd) {
                         modifiedSentence = inData.readUTF();
                         // ........................................
-                        // ........................................
                     }
 
                     welcomeData.close();
                     dataSocket.close();
                     System.out.println("\nWhat would you like to do next:\n retr: file.txt ||stor: file.txt  || close");
-                } else if (sentence.startsWith("retr: ")) {
-                    // ....................................................
-                } else if (sentence.startsWith("stor: ")) {
+                } else if (sentence.startsWith("RETR ")) {
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
 
-                } else if (sentence.equals("close: ")) {
+                    ServerSocket welcomeData = new ServerSocket(port);
+                    Socket dataSocket = welcomeData.accept();
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
 
+                    // ......................
+                    dataSocket.close();
+                } else if (sentence.startsWith("STOR ")) {
+                    port = port + 2;
+                    outToServer.writeBytes(port + " " + sentence + " " + '\n');
+
+                    ServerSocket welcomeData = new ServerSocket(port);
+                    Socket dataSocket = welcomeData.accept();
+                    DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+
+                    // ......................
+                    dataSocket.close();
+                } else if (sentence.equals("QUIT ")) {
+                    isOpen = false;
+                    clientgo = false;
+
+                    System.out.println("Client is disconnecting");
+                    controlSocket.close();
                 } else {
-                    System.out.println(
-                            "\nCommand not recognized try: \n list: || retr: file.txt ||stor: file.txt  || close");
+                    System.out
+                            .println("\nCommand not recognized try: \n LIST || RETR file.txt ||STOR file.txt  || quit");
                 }
             }
         } else {
