a94515d4cc0141a1e9a9cbd87f0923d12fe08035
derrikf2 <fleminde@mail.gvsu.edu>
2018-11-11 15:12:04 -0500

    update

--- projects/project02/gvnapster/server/src/main/java/CentralServer.java
+++ projects/project02/gvnapster/server/src/main/java/CentralServer.java
@@ -11,25 +11,25 @@ public class CentralServer {
         // for verifying build working
         System.out.println("HEY! The CentralServer is running");
 
-        //testing
-        CentralServerRequest request = new CentralServerRequest();
+//        //testing
+//        CentralServerRequest request = new CentralServerRequest();
 
 
-//        // control port
-//        int port = 9381;
-//        // Establish the listen socket.
-//        ServerSocket socket = new ServerSocket(port);
-//        // Process HTTP service requests in an infinite loop.
-//        while (!socket.isClosed()) {
-//            // Listen for a TCP connection request.
-//            Socket connection = socket.accept();
-//            // Construct an object to process the HTTP request message.
-//            CentralServerRequest request = new CentralServerRequest(connection);
-//            // Create a new thread to process the request.
-//            Thread thread = new Thread(request);
-//            // Start the thread.
-//            thread.start();
-//        }
-//        socket.close();
+        // control port
+        int port = 9381;
+        // Establish the listen socket.
+        ServerSocket socket = new ServerSocket(port);
+        // Process HTTP service requests in an infinite loop.
+        while (!socket.isClosed()) {
+            // Listen for a TCP connection request.
+            Socket connection = socket.accept();
+            // Construct an object to process the HTTP request message.
+            CentralServerRequest request = new CentralServerRequest(connection);
+            // Create a new thread to process the request.
+            Thread thread = new Thread(request);
+            // Start the thread.
+            thread.start();
+        }
+        socket.close();
     }
 }
